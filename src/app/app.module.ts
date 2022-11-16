2a5724372ec8ec69beaf45b664db36a427268062
ksp1510 <42215711+ksp1510@users.noreply.github.com>
2022-11-15 22:24:15 -0500

    Assignment2 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { MyAlbumsComponent } from './my-albums/my-albums.component';
+import { CreateAlbumComponent } from './create-album/create-album.component';
+import { AlbumDetailsComponent } from './album-details/album-details.component';
+import { UploadPicturesComponent } from './upload-pictures/upload-pictures.component';
+import { PhotoDetailsComponent } from './photo-details/photo-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent,
+    MyAlbumsComponent,
+    CreateAlbumComponent,
+    AlbumDetailsComponent,
+    UploadPicturesComponent,
+    PhotoDetailsComponent
   ],
   imports: [
     BrowserModule,
